c587e61ce155696503855f025b24ca0d39f59a3f
mdmarufprodhan <marufprodhan463@gmail.com>
2022-09-09 00:35:51 +0600

    Initial Commit

---
+++ SwiftUIApiCall/SwiftUIApiCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIApiCallApp.swift
+//  SwiftUIApiCall
+//
+//  Created by Md Maruf Prodhan on 9/9/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIApiCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
